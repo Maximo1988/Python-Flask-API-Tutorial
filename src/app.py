b1fa5ff0b6654b110143c3cf7306b585457d60a1
Maximo1988 <max_bd88@hotmail.com>
2022-03-31 19:03:27 +0000

    actualizacion de ejercicios

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask import request
 app = Flask(__name__)
 todos=[{"label": "My first task", "done": False}]
+add_new_todo=[{"label":"My second task", "done": False}]
 
 @app.route('/todos', methods=['GET'])
 def task():
@@ -14,7 +15,7 @@ def add_new_todo():
     return 'Response for the POST todo'
 
 import json
-decoded_object = json.loads(original_string)
+decoded_object = json.loads(request.data)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
